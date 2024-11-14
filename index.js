3ec0e29b1bad6d3d46cac8e97fa7d0eb078c7d90
gfcf14 <carloscuba014@gmail.com>
2024-11-14 12:36:26 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// increase this amount the more files there are
+const TOTAL_FILES = 2;
+
+$(() => {
+  const favTable = document.querySelector('#fav-table');
+
+  for (let i = TOTAL_FILES; i > 0; i--) {
+    $.getJSON(`./saved-${i}.json`, (data) => {
+      data.forEach(favObj => {
+        const { icon, title, content, footer } = favObj;
+        const imgColContent = `<a href='${icon.url}'><img src='${icon.src}' /></a>`;
+        const textColContent = `<a href='${icon.url}'><div class='title'>${title}</div></a><br />${content}<a href='${footer.link}'><div class='comments'>${footer.type == 'C' ? 'comments' : 'context'}</div></a>`;
+
+        const favRow = document.createElement('tr');
+        const imgCol = document.createElement('td');
+        const textCol = document.createElement('td');
+
+        imgCol.classList.add('topped');
+
+        imgCol.appendChild(document.createRange().createContextualFragment(imgColContent));
+        textCol.appendChild(document.createRange().createContextualFragment(textColContent));
+
+        favRow.appendChild(imgCol);
+        favRow.appendChild(textCol);
+        favTable.appendChild(favRow);
+      });
+    });
+  }
+});
